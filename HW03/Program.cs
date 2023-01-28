a2a3e248f9548897e8277add5c531adb74a23f9a
Anastasiya <atassmorodina@gmail.com>
2023-01-28 07:46:07 +0300

    Третья домашка done

---
+++ HW03/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным 
+и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+double[] InitArray(double dimension)
+{
+    double[] arr = new double[10];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(1, 100);
+    }
+
+    return arr;
+}
+
+Console.WriteLine();
+double[] array = InitArray(10);
+Console.WriteLine(string.Join(", ", array));
+double Min = array.Min();
+double Max = array.Max();
+double diff = Max - Min;
+Console.WriteLine($"-> В указанном массиве разница между максимальным числом {Max} и минимальным числом {Min} составила {diff}");
